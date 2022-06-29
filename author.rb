362a0f71b6bfa9709208f0d595321a3494843fd8
Have Samuel <samiehave9@gmail.com>
2022-06-29 20:07:27 +0300

    Fix Linters

--- author.rb
+++ author.rb
@@ -1,7 +1,7 @@
 require_relative 'item'
 
 class Author
-  attr_accessor :first_name, :last_name, :items = []
+  attr_accessor :first_name, :last_name, :items
   
   def initialize(first_name, last_name)
     @id = id
