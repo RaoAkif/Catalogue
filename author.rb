f275475ce27aec29757a1b8581cd2cf81c6c1598
Have Samuel <samiehave9@gmail.com>
2022-06-29 16:22:54 +0300

    Deleted a few errors

--- author.rb
+++ author.rb
@@ -1,11 +1,13 @@
+require_relative 'item'
+
 class Author
   attr_accessor :first_name, :last_name, :items = []
+  
   def initialize(first_name, last_name)
     @id = id
     @first_name = first_name
     @last_name = last_name
     @items = []
-    add_author
   end
 
   def add_item(item)
