469e6354bd4c522ca4baa758ce438c73ecf6ca16
Ivan Gaona <ivaan.lg.92@gmail.com>
2022-06-29 15:14:01 -0500

    Fix linters issues

--- music_album.rb
+++ music_album.rb
@@ -4,7 +4,7 @@ class MusicAlbum < Item
   attr_reader :on_spotify, :title
 
   def initialize(title, genre, publish_date, archived: false, on_spotify: true)
-    super(title, publish_date, archived:)
+    super(title, publish_date)
     @on_spotify = on_spotify
     add_genre(genre)
   end
