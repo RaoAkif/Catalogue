8625044dc76a1b6547e8ac4bac87b324eb526d7c
Have Samuel <samiehave9@gmail.com>
2022-06-29 19:02:08 +0300

    Created methods in the game.rb

---
+++ game.rb
@@ -0,0 +1,25 @@
+require 'item'
+require 'date'
+
+class Game < Item
+  attr_accessor :multiplayer: true last_played_at
+
+  def initialize(title, publish_date, last_played_at, multiplayer: true, archeived: false)
+    super(title, publish_date)
+    @multiplayer = multiplayer
+    @last_played_at = DateTime.parse(last_played_at)
+  end
+
+  def can_be_archived?
+    today = DateTime.now
+    age_in_days = today - @last_played_at
+    age_in_years = age_in_days.to_i / 365.25
+    return true if super && age_in_years > 2
+    false
+  end
+
+  def to_hash
+    { title: @title, publish_date: @publish_date, last_played_at: @last_played_at.strftime('%Y-%m-%d'), multiplayer: @multiplayer }
+  end
+
+end
